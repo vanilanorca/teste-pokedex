1c0d32685b38569eb99ccf9fe57fd86b87e1b243
vanilanorca <vilopesalves@hotmail.com>
2022-02-22 02:03:07 -0300

    .

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/pokedex-synvia"));
+app.use(
+  express.static(path.join(__dirname, "../client/dist/pokedex-synvia"), {
+    maxAge: "1y",
+  })
+);
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/pokedex-synvia/" })
+app.get("*", (req, res) =>
+  res.sendFile(__dirname, "../client/dist/pokedex-synvia/index.html")
 );
 
 // Start the app by listening on the default Heroku port
